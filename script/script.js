929c04632af324cd04e02e4db66c43ccf3410198
Kirill Korneev <kirill.corneew2017@yandex.ru>
2020-05-17 13:42:24 +0300

    first-rev

--- script/script.js
+++ script/script.js
@@ -1,36 +1,46 @@
 // Находим форму в DOM
-let formElement = document.querySelector('.pop'); //вся форма
+let formElement = document.querySelector('.popup'); //попап
+let form = document.querySelector('.form'); //форма
 let profileEdit = document.querySelector('.profile__edit'); //кнопка редактирования
-let formCloseButton = formElement.querySelector('.edit__close'); //кнопка закрытие формы
+let formCloseButton = formElement.querySelector('.popup__close'); //кнопка закрытие формы
 let profileName = document.querySelector('.profile__name'); //имя в профиле
 let profileJob = document.querySelector('.profile__about');  //подпись в профиле
 let nameInput = formElement.querySelector('.form__input_el_name'); //имя в форме
 let jobInput = formElement.querySelector('.form__input_el_spec'); //подпись в форме
 let formButton = formElement.querySelector('.form__button'); //кнопка сохранить
 
-
+///Функция отправки формы
 function formSubmitHandler (evt) {
-    evt.preventDefault(); 
     
+    evt.preventDefault();
+
+    let valueName = nameInput.value;
+    let valueJob = jobInput.value;
+    profileName.textContent = valueName;
+    profileJob.textContent = valueJob;
+    
+    formOpenClose(); 
 }
 
+///Функция закрытия формы, без отправки
+function formClose() {
+    formElement.classList.toggle('popup_close');
+}
+
+///Фнкция закрытия формы, с отправкой
 function formOpenClose() {
-    formElement.classList.toggle('pop_open');
+
+    let textName = profileName.textContent;
+    let textJob = profileJob.textContent;
+    nameInput.setAttribute("value", textName);
+    jobInput.setAttribute("value", textJob);
+
+    formElement.classList.toggle('popup_close');
 }
 
-function formChange() {
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    formOpenClose();
-}
 
+profileEdit.addEventListener('click', formOpenClose); ///открытие формы
 
-profileEdit.addEventListener('click', formOpenClose);
+form.addEventListener('submit', formSubmitHandler); ///отправка формы
 
-nameInput.setAttribute("value", profileName.textContent);
-jobInput.setAttribute("value", profileJob.textContent);
-
-formElement.addEventListener('submit', formSubmitHandler);
-
-formCloseButton.addEventListener('click', formOpenClose);
-formButton.addEventListener('click', formChange);
+formCloseButton.addEventListener('click', formClose); ///закрытие без отправки
