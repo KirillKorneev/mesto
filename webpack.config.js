ca66e09d54ac609b6484c16ddc169487a38df331
KirillKorneev <45182441+KirillKorneev@users.noreply.github.com>
2020-08-14 12:01:46 +0300

    fix\dir

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path'); // подключаем path к конфигу вебпак
 
 module.exports = {
-    entry: { main: './script/index.js' },
+    entry: { main: "./src/script/pages/index.js" },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
